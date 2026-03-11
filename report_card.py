ea5b4967b394ddf424a7cc5e0a9f2ab14143e3c4
saurabh7869721 <saurabhshrivastava1486@gmail.com>
2026-03-11 19:13:34 +0530

    python studend report card project using format_method

--- report_card.py
+++ report_card.py
@@ -1,1 +1,27 @@
+# Student details
+name = input("Enter student name: ")
+roll_no = input("Enter roll number: ")
+
+# Marks input
+math = int(input("Math marks: "))
+science = int(input("Science marks: "))
+english = int(input("English marks: "))
+
+# Total & Percentage
+total = math + science + english
+percentage = total / 3
+
+# Report Card
+report = """
+------ Report Card ------
+Name      : {}
+Roll No   : {}
+Math      : {}
+Science   : {}
+English   : {}
+Total     : {}
+Percentage: {:.2f}%
+""".format(name, roll_no, math, science, english, total, percentage)
+
+print(report)
 
