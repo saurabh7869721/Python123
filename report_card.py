cfd110a896d42c3baa71064fd93669022ed71cf8
saurabh7869721 <saurabhshrivastava1486@gmail.com>
2026-03-11 22:04:56 +0530

    student report card using f-string method

--- report_card.py
+++ report_card.py
@@ -1,3 +1,6 @@
+
+#studenr report card.
+
 # Student details
 name = input("Enter student name: ")
 roll_no = input("Enter roll number: ")
@@ -24,4 +27,49 @@ Percentage: {:.2f}%
 """.format(name, roll_no, math, science, english, total, percentage)
 
 print(report)
+
+
+
+
+#student report using if elif  else.
+
+
+name = input("Enter Student Name: ")
+roll_no = input("Enter Roll Number: ")
+maths = int(input("Enter Maths Marks: "))
+science = int(input("Enter Science Marks: "))
+english = int(input("Enter English Marks: "))
+
+
+total = maths + science + english
+average = total / 3
+percentage = (total / 300) * 100
+
+
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 75:
+    grade = "A"
+elif percentage >= 60:
+    grade = "B"
+elif percentage >= 50:
+    grade = "C"
+else:
+    grade = "F"
+
+
+print("\n_______report  card_______")
+
+print(f"Name        :{name}")
+print(f"Roll No     :{roll_no}")
+print(f"Maths       :{maths}")
+print(f"Science     :{science}")
+print(f"English     :{english}")
+
+
+print(f"Total       :{total}")
+print(f"Average     :{average}")
+print(f"Percentage  :{percentage:.2f}%")
+print(f"Grade       :{grade}")
+
 
