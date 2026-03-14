d1c197afaf400e2b57511725e12e05043873428d
saurabh7869721 <saurabhshrivastava1486@gmail.com>
2026-03-14 23:19:41 +0530

    add escape sequences.

---
+++ escape_methods.py
@@ -0,0 +1,16 @@
+#print program using \n.
+
+print("Hello\nworld")
+
+#program using \t.
+
+print("Name\tAge\tCity")
+print("Name:     rahul\nCourse:   B.com\nSubject:  Python")
+#print program using\\.
+
+print("\\users\\saurabh\\documents")
+
+#print python using \".
+
+print("HE said \"python is easy\"")
+print('programmers sugest \'python\'for programming.\nEven\"Cisco\" also uses python for programming.')
