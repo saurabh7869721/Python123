e3c91f5434ee68e5e2e14907066e49b514a8f646
saurabh7869721 <saurabhshrivastava1486@gmail.com>
2026-03-15 16:45:04 +0530

    add string indexing nethod

---
+++ string_indexing.py
@@ -0,0 +1,32 @@
+#print  first and last word.
+
+name=input("Enter your name :")
+print("first letter:",name[0])
+print("second letter:",name[-1])
+
+#print first digit.
+
+phone=input("Enter your number:")
+print("first digit",phone[0])
+
+#print second word.
+
+name=input("Enter your name :")
+print("second word:",name[1])
+
+#print middle word.
+
+word=input("Enter your word:")
+middle=(len(word)//2)
+print("middle character:",word[middle])
+
+#print last word.
+
+city=input("Enter name :")
+print(city[-1])
+
+#print first letter of password in print.
+
+password=input("Enter your password:")
+print("password hint",password[0]+"*****")
+ 
