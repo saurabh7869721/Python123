af16682a2f4631a51200711f76e91cfa15d47c64
saurabh7869721 <saurabhshrivastava1486@gmail.com>
2026-03-18 16:44:05 +0530

    add pyament status checker

---
+++ string_methods.py
@@ -0,0 +1,18 @@
+#len()
+#1
+
+response=["yes","No","maybe"]
+for r in response:
+    print(r,len('r'))
+
+#2
+
+employee="EMP20260316"
+print(employee,len(employee))
+
+payment=int(input("Enter your payment"))
+
+if payment ==8000:
+    print("paid")
+else:
+    print("pending")
